9aef3f44858e1e2f8b284602606075b881f033ec
Olivier Deprez <olivier.deprez@arm.com>
2023-09-27 12:36:27 +0200

    qemu: set default makefile opts for sel2 spmc
    NOT FOR MERGE
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>

--- qemu_v8.mk
+++ qemu_v8.mk
@@ -1,3 +1,7 @@
+SPMC_AT_EL=2
+MEMTAG=y
+PAUTH=y
+
 ################################################################################
 # Following variables defines how the NS_USER (Non Secure User - Client
 # Application), NS_KERNEL (Non Secure Kernel), S_KERNEL (Secure Kernel) and
